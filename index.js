7a85187f25893adf70cfa5006b8db0a627cb8038
Idancok <104585215+Idancok@users.noreply.github.com>
2022-10-22 04:27:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @jall strore
+› Base Ori @jallbotz
 › Kalau mau ubah/edit owner di settings.js
 › Edit text menu di file language › help.js
 */
